8f71eb55c862e77508676dd907f4e84bf6f82d89
LJPCODESTUDIO <89280590+LJPCODESTUDIO@users.noreply.github.com>
2024-12-02 21:22:30 -0700

    More work, though still quite a long way till it's finished.

--- MurderMystery.cs
+++ MurderMystery.cs
@@ -6,6 +6,7 @@ using AMP.Network.Data;
 using AMP.Network.Packets.Implementation;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
 
 namespace MurderMystery
@@ -14,7 +15,7 @@ namespace MurderMystery
     {
         public override string NAME => "MurderMystery";
         public override string AUTHOR => "LetsJustPlay";
-        public override string VERSION => "0.1";
+        public override string VERSION => "0.2";
 
         private bool gameRunning = false;
 
@@ -25,7 +26,10 @@ namespace MurderMystery
 
         private List<ClientData> players = new List<ClientData>();
         private List<ClientData> citizens = new List<ClientData>();
-        private List<ClientData> dead = new List<ClientData>();
+        private List<ClientData> deadPlayers = new List<ClientData>();
+
+        private CancellationTokenSource cancellationTokenSource;
+        private CancellationToken cancellationToken;
 
 
         internal class MurderMysteryConfig : PluginConfig
@@ -36,8 +40,49 @@ namespace MurderMystery
         }
 
 
+        private void deadTick(CancellationToken cancellationToken)
+        {
+            while (gameRunning && !cancellationToken.IsCancellationRequested)
+            {
+                List<ClientData> localClients = deadPlayers;
+                int[] deadPlayerIds = localClients.Select(i => i.ClientId).ToArray();
+                foreach (ClientData client in localClients)
+                {
+                    Vector3 pos = client.player.Position;
+                    pos.y += 1000;
+                    ModManager.serverInstance.netamiteServer.SendToAllExcept(
+                        new PlayerPositionPacket(
+                            playerId: client.ClientId,
+                            handLeftPos: new Vector3(),
+                            handLeftRot: new Vector3(),
+                            handRightPos: new Vector3(),
+                            handRightRot: new Vector3(),
+                            headPos: new Vector3(),
+                            headRot: new Vector3(),
+                            playerPos: pos,
+                            playerRot: 0
+                        ),
+                        deadPlayerIds
+                    );
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void matchLoop()
+        {
+
+        }
+
+        private void intermissionLoop()
+        {
+
+        }
+
         public override void OnStart()
         {
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
             ServerEvents.onPlayerJoin += OnPlayerJoin;
             ServerEvents.onPlayerQuit += OnPlayerQuit;
             config = (MurderMysteryConfig)GetConfig();
@@ -48,11 +93,27 @@ namespace MurderMystery
             players.Add(client);
             if (gameRunning)
             {
-                dead.Add(client);
+                deadPlayers.Add(client);
+                client.SetDamageMultiplicator(0);
                 ModManager.serverInstance.netamiteServer.SendTo(
                     client,
                     new DisplayTextPacket("deadNotify", "You have joined during a match and have been automatically murdered.", Color.white, new Vector3(0,0,2), true, true, 5)
                 );
+            }
+            else
+            {
+                ModManager.serverInstance.netamiteServer.SendToAll(
+                    new DisplayTextPacket("playerJoinNotify", $"{client.ClientName} has joined the server.\n{players.Count}/{config.requiredPlayerCount}", Color.white, new Vector3(0,0,2), true, true, 1)
+                );
+
+                Thread.Sleep(1000);
+
+                if (players.Count >= config.requiredPlayerCount)
+                {
+                    ModManager.serverInstance.netamiteServer.SendToAll(
+                        new DisplayTextPacket("matchStart", $"Enough players have joined. Starting game.", Color.white, new Vector3(0, 0, 2), true, true, 1)
+                    );
+                }
             }
         }
 
@@ -68,7 +129,7 @@ namespace MurderMystery
             {
                 try
                 {
-                    dead.Remove(dead.Where(i => i.ClientId == client.ClientId).First());
+                    deadPlayers.Remove(deadPlayers.Where(i => i.ClientId == client.ClientId).First());
                 }
                 catch { }
             }
